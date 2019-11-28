7b4df1e3b8fb2b32293920f1ce9fc993f2eaae07
Niko Gontjarow <niko.gontjarow@gmail.com>
2019-11-28 13:34:35 +0200

    Forgot to throw in the test file for lcm.

---
+++ test/lcm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../libft.h"
+
+int main()
+{
+	printf("%d\n", ft_lcm(10, 12)); // 60
+	printf("%u\n", ft_lcm(12, 75)); // 300
+	printf("%u\n", ft_lcm(45, 120)); // 360
+}
