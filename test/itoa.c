a105a1e5e11a0e6cc925c7cf3d763f490987c931
ngontjar <54755292+Gontjarow@users.noreply.github.com>
2020-02-28 20:44:11 +0200

    Rewrote ft_itoa in a way that's actually readable. Added test.

--- test/itoa.c
+++ test/itoa.c
@@ -1,34 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   itoa.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/21 15:44:27 by ngontjar          #+#    #+#             */
-/*   Updated: 2020/02/28 20:39:29 by ngontjar         ###   ########.fr       */
+/*   Created: 2019/11/28 14:26:38 by ngontjar          #+#    #+#             */
+/*   Updated: 2020/02/28 20:41:13 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdio.h>
+#include <limits.h>
+#include "../libft.h"
 
-char	*ft_itoa(int num)
+int main()
 {
-	char			string[11 + 1];
-	unsigned int	number;
-	unsigned int	length;
-
-	if (num == 0)
-		return (ft_strdup("0"));
-	ft_memset(string, 0, 12);
-	string[0] = (num < 0 ? '-' : '\0');
-	length = ft_numlen(num);
-	number = ABS(num);
-	while (length--)
-	{
-		if (number)
-			string[length] = '0' + (number % 10);
-		number /= 10;
-	}
-	return (ft_strdup(string));
+	printf("0 = %s\n", ft_itoa(0));
+	printf("1 = %s\n", ft_itoa(1));
+	printf("-1 = %s\n", ft_itoa(-1));
+	printf("2147483647 = %s\n", ft_itoa(INT_MAX));
+	printf("-2147483648 = %s\n", ft_itoa(INT_MIN));
 }
